0e3c256bf18d2d082aae63ebd4a9f78b54179386
maniacbug <maniacbug@ymail.com>
2011-11-09 19:31:55 -0800

    Removed rtcevent, its in tictocs now

--- RtcEvent.cpp
+++
@@ -1,9 +0,0 @@
-// STL includes
-// C includes
-// Library includes
-// Project includes
-#include <RtcEvent.h>
-
-/****************************************************************************/
-
-// vim:cin:ai:sts=2 sw=2 ft=cpp
