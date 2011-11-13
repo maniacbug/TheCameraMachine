9e121f89a1328525d378659232a5a4c1eb44adc4
maniacbug <maniacbug@ymail.com>
2011-11-13 07:17:30 -0800

    Stub files for LancControl class

---
+++ LancControl.h
@@ -0,0 +1,21 @@
+#ifndef __LANCCONTROL_H__
+#define __LANCCONTROL_H__
+
+// STL includes
+// C includes
+// Library includes
+// Project includes
+
+/**
+ * Example for how classes should be declared
+ */
+
+class LancControl
+{
+private:
+protected:
+public:
+};
+
+#endif // __LANCCONTROL_H__
+// vim:cin:ai:sts=2 sw=2 ft=cpp
