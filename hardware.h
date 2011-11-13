bf7d75c6f362129dccdc0ff8d2371950334ff3e4
maniacbug <maniacbug@ymail.com>
2011-11-13 13:57:44 -0800

    Refactor rtc setter to require a leading 'T' instead of a dedicated pin.

--- hardware.h
+++ hardware.h
@@ -10,7 +10,6 @@ const int power_relay_pin = 9;
 const int alt_relay_pin = 4;
 const int lanc_command_pin = A0;
 const int lanc_data_pin = A1;
-const int clock_set_pin = 13;  // When low, monitor serial for clock changes
 
 #endif // __HARDWARE_H__
 // vim:cin:ai:sts=2 sw=2 ft=cpp
