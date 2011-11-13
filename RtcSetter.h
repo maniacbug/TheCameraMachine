44ed2bcae6bbe6854f56a4c4d94f2503eb2fb3f5
maniacbug <maniacbug@ymail.com>
2011-11-12 22:07:18 -0800

    Stubs for new RtcSetter class

---
+++ RtcSetter.h
@@ -0,0 +1,21 @@
+#ifndef __RTCSETTER_H__
+#define __RTCSETTER_H__
+
+// STL includes
+// C includes
+// Library includes
+// Project includes
+
+/**
+ * Example for how classes should be declared
+ */
+
+class RtcSetter
+{
+private:
+protected:
+public:
+};
+
+#endif // __RTCSETTER_H__
+// vim:cin:ai:sts=2 sw=2 ft=cpp
