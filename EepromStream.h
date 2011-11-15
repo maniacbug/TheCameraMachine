ef2b7cc225ca0a4e2b2fe24e9645936160028b55
maniacbug <maniacbug@ymail.com>
2011-11-14 21:34:54 -0800

    Added overflow detection to eeprom stream

--- EepromStream.h
+++ EepromStream.h
@@ -24,38 +24,60 @@ class EepromStream
 {
 private:
   unsigned current_address;
+  static const unsigned end_address = 1024; // Atmega328p
+  bool overflow;
 protected:
+  // Handle wrapping the address around the end
+  unsigned next(unsigned& current)
+  {
+    unsigned result = current++;
+    while ( current >= end_address )
+    {
+      current -= end_address;
+      overflow = true;
+    }
+    return result;
+  }
 public:
-  EepromStream(void): current_address(0) {}
+  EepromStream(void): current_address(0), overflow(false) {}
   template <class T> void write(const T& object) 
   {
+    overflow = false;
     int i = sizeof(T);
     const uint8_t* current = reinterpret_cast<const uint8_t*>(&object);
     while (i--)
-      EEPROM.write(current_address++,*current++);
+      EEPROM.write(next(current_address),*current++);
   }
   template <class T> void read(T& object)
   {
+    overflow = false;
     int i = sizeof(T);
     uint8_t* current = reinterpret_cast<uint8_t*>(&object);
     while (i--)
-      *current++ = EEPROM.read(current_address++);
+      *current++ = EEPROM.read(next(current_address));
   }
   template <class T> void peek(T& object)
   {
+    overflow = false;
     unsigned address = current_address;
     int i = sizeof(T);
     uint8_t* current = reinterpret_cast<uint8_t*>(&object);
     while (i--)
-      *current++ = EEPROM.read(address++);
+      *current++ = EEPROM.read(next(address));
   }
   void seek( unsigned to )
   {
     current_address = to;
+    while ( current_address >= end_address )
+      current_address -= end_address;
   }
   unsigned tell( void ) const
   {
     return current_address;
+  }
+  bool didOverflow( void ) const
+  {
+    return overflow;
   }
 };
 
