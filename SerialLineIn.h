292e93dc66b476de6df6984d6833957404e51a1a
maniacbug <maniacbug@ymail.com>
2011-11-18 07:15:27 -0800

    Added stubs for SerialLineIn

---
+++ SerialLineIn.h
@@ -0,0 +1,29 @@
+/*
+ Copyright (C) 2011 J. Coliz <maniacbug@ymail.com>
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
+ */
+
+#ifndef __SERIALLINEIN_H__
+#define __SERIALLINEIN_H__
+
+// STL includes
+// C includes
+// Library includes
+// Project includes
+
+/**
+ * Example for how classes should be declared
+ */
+
+class SerialLineIn
+{
+private:
+protected:
+public:
+};
+
+#endif // __SERIALLINEIN_H__
+// vim:cin:ai:sts=2 sw=2 ft=cpp
