b4ed1b78dad05a6245f281bcd155e7fed7c049b4
maniacbug <maniacbug@ymail.com>
2012-02-17 06:14:05 -0800

    Add copyright blocks on files which lacked it.

--- objects.cpp
+++ objects.cpp
@@ -1,3 +1,11 @@
+/*
+ Copyright (C) 2012 J. Coliz <maniacbug@ymail.com>
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
+ */
+
 // Framework includes
 #if ARDUINO < 100
 #include <WProgram.h>
