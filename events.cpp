c0699ee2c86a2993ab9d97ee597d255d8687b086
maniacbug <maniacbug@ymail.com>
2012-02-18 12:58:11 -0800

    events

--- events.cpp
+++ events.cpp
@@ -16,10 +16,14 @@
 
 const RtcEvTable::evline events_table[] PROGMEM = {
   //YY,MM,DD HH MM SS,CH, signal
-  { 12, 2,19, 7, 0, 0, 0,signal_fire_camera },
-  { 12, 2,19, 7,30, 0, 0,signal_fire_camera },
-  { 12, 2,19, 8, 0, 0, 0,signal_fire_camera },
-  { 12, 2,19, 8,30, 0, 0,signal_fire_camera },
+  { 12, 2,19,13, 0, 0, 0,signal_fire_camera },
+  { 12, 2,19,14, 0, 0, 0,signal_fire_camera },
+  { 12, 2,19,15, 0, 0, 0,signal_fire_camera },
+  { 12, 2,19,16, 0, 0, 0,signal_fire_camera },
+  { 12, 2,19,17, 0, 0, 0,signal_fire_camera },
+  { 12, 2,19,18, 0, 0, 0,signal_fire_camera },
+  { 12, 2,19,19, 0, 0, 0,signal_fire_camera },
+  { 12, 2,19,20, 0, 0, 0,signal_fire_camera },
   
 };
 int num_events = sizeof(events_table)/sizeof(RtcEvTable::evline);
